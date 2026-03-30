c2c5c80d53c3b030d97b697b98adb5de85dd2718
Tha-liaa <134466038+Tha-liaa@users.noreply.github.com>
2026-03-29 21:37:14 -0500

    Create MiddleAgedDriver.cpp

---
+++ MiddleAgedDriver.cpp
@@ -0,0 +1,9 @@
+#include "MiddleAgedDriver.h"
+
+MiddleAgedDriver::MiddleAgedDriver() {}
+
+MiddleAgedDriver::MiddleAgedDriver(string name, string workCity, Date dob)
+    : Driver(name, workCity, dob) {}
+
+void MiddleAgedDriver::operations() {
+}
